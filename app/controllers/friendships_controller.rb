b842ccb6ff1733e31d35d8a587feec829256073b
batgnome <fouflord@gmail.com>
2022-04-18 09:01:31 -0700

    added stopped following friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,13 @@
 class FriendshipsController < ApplicationController
-        def destroy
-        friend = User.find(params[:id])
-        friendship = Friendship.where(user_id: current_user.id, friend_id: friend.id).first
+    def create
+        
+    end
+
+    def destroy
+        
+        friendship = current_user.friendships.where(friend_id: params[:id]).first
         friendship.destroy
-        flash[:notice] = "You have stopped following #{friend}"
+        flash[:notice] = "Stopped following"
         redirect_to friends_path
     end
 end
